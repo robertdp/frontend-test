6720c0f6657e81fd6de0e0d236b63a7c637e4ace
Robert Porter <porter_d@worksap.co.jp>
2018-11-08 15:17:37 +0900

    SaleId needed instances of Ord and Encode for modelling favorites

--- src/Data/Sale.purs
+++ src/Data/Sale.purs
@@ -4,8 +4,8 @@ import Prelude
 
 import Data.Generic.Rep (class Generic)
 import Data.Sale.Image (SaleImage)
-import Foreign.Class (class Decode)
-import Foreign.Generic (defaultOptions, genericDecode)
+import Foreign.Class (class Decode, class Encode)
+import Foreign.Generic (defaultOptions, genericDecode, genericEncode)
 
 
 newtype Sale = Sale
@@ -23,8 +23,12 @@ instance decodeSale :: Decode Sale where
 newtype SaleId = SaleId String
 
 derive newtype instance eqSaleId :: Eq SaleId
+derive newtype instance ordSaleId :: Ord SaleId
 
 derive instance genericSaleId :: Generic SaleId _
 
 instance decodeSaleId :: Decode SaleId where
   decode = genericDecode $ defaultOptions { unwrapSingleConstructors = true }
+
+instance encodeSaleId :: Encode SaleId where
+  encode = genericEncode $ defaultOptions { unwrapSingleConstructors = true }
