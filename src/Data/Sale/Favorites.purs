5101bea984f85d28cadc266b6169364991f13af2
Robert Porter <porter_d@worksap.co.jp>
2018-11-08 17:06:57 +0900

    Hid the implementation of FavoriteSales. Cheap encapsulation.

--- src/Data/Sale/Favorites.purs
+++ src/Data/Sale/Favorites.purs
@@ -1,7 +1,16 @@
-module Data.Sale.Favorites where
+module Data.Sale.Favorites
+  ( FavoriteSales
+  , empty
+  , insert
+  , delete
+  , member
+  )
+  where
 
 import Prelude
 
+import Data.Newtype (class Newtype)
+import Data.Newtype as Newtype
 import Data.Sale (SaleId)
 import Data.Set (Set)
 import Data.Set as Set
@@ -9,6 +18,8 @@ import Foreign.Class (class Decode, class Encode, decode, encode)
 
 
 newtype FavoriteSales = FavoriteSales (Set SaleId)
+
+derive instance newtypeFavoriteSales :: Newtype FavoriteSales _
 
 instance decodeFavoriteSales :: Decode FavoriteSales where
   decode = decode >>> map fromArray
@@ -24,3 +35,12 @@ instance encodeFavoriteSales :: Encode FavoriteSales where
 
 empty :: FavoriteSales
 empty = FavoriteSales Set.empty
+
+insert :: SaleId -> FavoriteSales -> FavoriteSales
+insert = Newtype.over FavoriteSales <<< Set.insert
+
+delete :: SaleId -> FavoriteSales -> FavoriteSales
+delete = Newtype.over FavoriteSales <<< Set.delete
+
+member :: SaleId -> FavoriteSales -> Boolean
+member id (FavoriteSales favs) = Set.member id favs
