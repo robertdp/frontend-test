ab85b74078d4f7763c0bb11b810fd14aeefa6f89
Robert Porter <porter_d@worksap.co.jp>
2018-11-08 15:18:27 +0900

    Here's the initial FavoriteSales implementation

---
+++ src/Data/Sale/Favorites.purs
@@ -0,0 +1,27 @@
+module Data.Sale.Favorites where
+
+import Prelude
+
+import Data.Generic.Rep (class Generic)
+import Data.Sale (SaleId)
+import Data.Set (Set)
+import Data.Set as Set
+import Foreign.Class (class Decode, class Encode, decode, encode)
+
+
+newtype FavoriteSales = FavoriteSales (Set SaleId)
+
+derive instance genericFavoriteSales :: Generic FavoriteSales _
+
+instance decodeFavoriteSales :: Decode FavoriteSales where
+  decode = decode >>> map fromArray
+    where
+      fromArray :: Array SaleId -> FavoriteSales
+      fromArray = Set.fromFoldable >>> FavoriteSales
+
+instance encodeFavoriteSales :: Encode FavoriteSales where
+  encode = toArray >>> encode
+    where
+      toArray :: FavoriteSales -> Array SaleId
+      toArray (FavoriteSales set) = Set.toUnfoldable set
+
