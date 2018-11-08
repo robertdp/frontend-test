b110ffc546b5d226669d7d8afcaee42d1fc184dc
Robert Porter <porter_d@worksap.co.jp>
2018-11-08 16:19:26 +0900

    Cleaned up the localStorage code a little

--- src/Control/App.purs
+++ src/Control/App.purs
@@ -11,11 +11,10 @@ import Control.Monad.Except (runExcept)
 import Control.Monad.Reader (class MonadAsk, class MonadReader, ReaderT, ask, runReaderT)
 import Data.Bifunctor (bimap)
 import Data.Either (Either(..), hush)
-import Data.Maybe (Maybe, fromMaybe, maybe)
+import Data.Maybe (Maybe, fromMaybe)
 import Data.Sale (Sale)
-import Data.Sale.Favorites (FavoriteSales(..))
+import Data.Sale.Favorites (FavoriteSales)
 import Data.Sale.Favorites as Favorites
-import Data.Set as Set
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Effect.Aff.Class (class MonadAff, liftAff)
@@ -23,6 +22,7 @@ import Effect.Class (class MonadEffect, liftEffect)
 import Foreign.Generic (decodeJSON, encodeJSON)
 import Web.HTML (window)
 import Web.HTML.Window as Window
+import Web.Storage.Storage (Storage)
 import Web.Storage.Storage as Storage
 
 
@@ -52,24 +52,29 @@ instance monadFetchSalesApp :: MonadFetch Unit (Array Sale) App where
         decodeJSON body
           # runExcept
           # bimap (FailedDecode <<< show) identity
+
       StatusCode 404, _ ->
         Left NotFound
+
       _, Left error ->
         Affjax.printResponseFormatError error
           # UnexpectedFormat
           # Left
+
       StatusCode status, _ ->
         UnexpectedStatus status
           # Left
 
+withLocalStorage :: forall m a. MonadEffect m => (Storage -> m a) -> m a
+withLocalStorage f = do
+  localStorage <- liftEffect $ Window.localStorage =<< window
+  f localStorage
+
 instance monadStorageFavoriteSalesApp :: MonadStorage FavoriteSales App where
-  store favs = do
-    localStorage <- liftEffect $ Window.localStorage =<< window
-    liftEffect $ Storage.setItem "favoriteSales" (encodeJSON favs) localStorage
+  store favs = withLocalStorage $ liftEffect <<< Storage.setItem "favoriteSales" (encodeJSON favs)
 
   retrieve = do
-    localStorage <- liftEffect $ Window.localStorage =<< window
-    json <- liftEffect $ Storage.getItem "favoriteSales" localStorage
+    json <- withLocalStorage $ liftEffect <<< Storage.getItem "favoriteSales"
     pure $ decode json
     where
       decode :: Maybe String -> FavoriteSales
