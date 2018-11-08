45d48770a24153dd9b6f63e5faa84bc9f55f302d
Robert Porter <porter_d@worksap.co.jp>
2018-11-08 16:42:12 +0900

    Since `web-html` is a dep again, moved back to using to to start React

--- src/Main.js
+++
@@ -1,4 +0,0 @@
-// This small FFI saves me from importing a large DOM library
-exports.document = function() {
-  return window.document;
-};
--- src/Main.purs
+++ src/Main.purs
@@ -9,7 +9,10 @@ import Effect (Effect)
 import Effect.Exception (throw)
 import React.Basic as React
 import React.Basic.DOM as DOM
-import Web.DOM.NonElementParentNode (NonElementParentNode, getElementById)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window as Window
 
 
 config :: Config
@@ -17,13 +20,9 @@ config = Config { baseURL: "/data" }
 
 main :: Effect Unit
 main = do
-  container <- getElementById "app" =<< document
+  container <- getElementById "app" =<< (map toNonElementParentNode $ Window.document =<< window)
   case container of
     Nothing -> throw "Container element not found."
     Just c  ->
       let app = React.element App.component { config }
       in DOM.render app c
-
-
-
-foreign import document :: Effect NonElementParentNode
