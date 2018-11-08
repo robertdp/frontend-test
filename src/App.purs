dc912690623d203eebe34dfbe63e274eb0213a9f
Robert Porter <porter_d@worksap.co.jp>
2018-11-08 17:07:22 +0900

    Starting work on the top-level component

--- src/App.purs
+++ src/App.purs
@@ -2,28 +2,26 @@ module App where
 
 import Prelude
 
-import Control.App (App(..), Config)
+import Control.App (Config)
 import Control.App as App
-import Control.Language.Fetch (FetchError, fetch)
-import Control.Language.Storage (retrieve, store)
-import Data.Either (Either)
-import Data.Sale (Sale, SaleId(..))
-import Data.Sale.Favorites (FavoriteSales(..))
-import Data.Sale.Favorites as Favorites
-import Data.Set as Set
+import Control.Language.Storage (retrieve)
+import Data.Sale.Favorites as FavoriteSales
 import Effect.Class (liftEffect)
-import Effect.Console (log)
-import Foreign.Generic (encodeJSON)
+import Network.RemoteData as RemoteData
 import React.Basic as React
 import React.Basic.DOM (text)
-import Unsafe.Coerce (unsafeCoerce)
 
 component :: React.Component { config :: Config }
 component = React.component { displayName: "App", initialState, receiveProps, render }
   where
-    initialState = {}
-    receiveProps { props } = App.run props.config do
-      (retrieve :: App FavoriteSales) >>= (encodeJSON >>> show >>> log >>> liftEffect)
-      store (FavoriteSales $ Set.singleton $ SaleId "test")
+    initialState =
+      { sales: RemoteData.NotAsked
+      , favorites: FavoriteSales.empty
+      }
+
+    receiveProps { props, setState } = App.run props.config do
+      favorites <- retrieve
+      liftEffect $ setState _ { favorites = favorites }
       pure unit
+
     render _ = text "App"
