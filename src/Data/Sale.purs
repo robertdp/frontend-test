94ce8947e25c78dbc79fac53ba3bf4d6d0c31943
Robert Porter <porter_d@worksap.co.jp>
2018-11-08 14:38:57 +0900

    Fleshed out the decodable data types

--- src/Data/Sale.purs
+++ src/Data/Sale.purs
@@ -1,14 +1,41 @@
 module Data.Sale where
 
 import Prelude
+
 import Data.Generic.Rep (class Generic)
+import Data.Sale.Image (SaleImage)
 import Foreign.Class (class Decode)
 import Foreign.Generic (defaultOptions, genericDecode)
 
 
-newtype Sale = Sale { name :: String }
+newtype Sale = Sale
+  { id :: SaleId
+  , name :: String
+  , description :: String
+  , images :: SaleImage
+  }
 
-derive instance genericSales :: Generic Sale _
+derive instance genericSale :: Generic Sale _
 
 instance decodeSale :: Decode Sale where
   decode = genericDecode $ defaultOptions { unwrapSingleConstructors = true }
+
+newtype SaleId = SaleId String
+
+derive newtype instance eqSaleId :: Eq SaleId
+
+derive instance genericSaleId :: Generic SaleId _
+
+instance decodeSaleId :: Decode SaleId where
+  decode = genericDecode $ defaultOptions { unwrapSingleConstructors = true }
+
+newtype SaleKey = SaleKey String
+
+derive newtype instance eqSaleKey :: Eq SaleKey
+
+derive instance genericSaleKey :: Generic SaleKey _
+
+instance decodeSaleKey :: Decode SaleKey where
+  decode = genericDecode $ defaultOptions { unwrapSingleConstructors = true }
+
+
