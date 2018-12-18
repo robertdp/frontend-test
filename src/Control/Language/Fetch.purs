0e617437bd950c0807f0c09d1da0f2f2823d4c4b
Robert Porter <porter_d@worksap.co.jp>
2018-12-18 17:19:18 +0900

    Added descriptive type alias for URL

--- src/Control/Language/Fetch.purs
+++ src/Control/Language/Fetch.purs
@@ -7,7 +7,9 @@ import Foreign.Class (class Decode)
 
 
 class Monad r <= MonadFetch r where
-  fetch :: forall a. Decode a => String -> r (FetchResult a)
+  fetch :: forall a. Decode a => URL -> r (FetchResult a)
+
+type URL = String
 
 type FetchResult = Either FetchError
 
